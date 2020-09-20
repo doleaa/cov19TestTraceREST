5d4f315d0d281b056568a92f3dd38bc56caeb471
adolea <dolea15@gmail.com>
2020-09-20 17:01:21 +0100

    --implement endpoint for creating new tests

--- src/services/tests/tests.class.ts
+++ src/services/tests/tests.class.ts
@@ -3,9 +3,45 @@ import { Service, MongoDBServiceOptions } from 'feathers-mongodb';
 import { Application } from '../../declarations';
 import createApplication from "@feathersjs/feathers";
 
-type TestStatus = "CREATED" | "SCHEDULED" | "SUBMITTED" | "DONE";
-type TestResult = "PENDING" | "NEGATIVE" | "POSITIVE";
+enum TestStatus {
+  CREATED = "CREATED",
+  SCHEDULED = "SCHEDULED",
+  SUBMITTED = "SUBMITTED",
+  DONE = "DONE"
+}
 
+enum TestResult {
+  PENDING = "PENDING",
+  NEGATIVE = "NEGATIVE",
+  POSITIVE = "POSITIVE"
+};
+
+interface NewTestDto {
+  testingCampaignID: string,
+  employeeIdentifier: number
+}
+
+interface CreatedTestDto {
+  _id: ObjectId,
+  testingCampaignID: ObjectId,
+  employeeIdentifier: number,
+  creationTimestamp: Date,
+  status: TestStatus,
+  result: TestResult
+
+}
+
+//Insert schema interface
+interface TestSchema {
+  _id?: ObjectId,
+  testingCampaignID: ObjectId,
+  employeeIdentifier: number,
+  creationTimestamp: Date,
+  status: TestStatus,
+  result: TestResult
+}
+
+//Read query interfaces
 interface TestDocumentQuery {
   testingCampaignID?: ObjectId,
   employeeIdentifier?: number,
@@ -13,7 +49,6 @@ interface TestDocumentQuery {
   status?: TestStatus,
   result?: TestResult
 }
-
 interface IndexedTestDocumentQuery extends TestDocumentQuery{
   _id?: ObjectId
 }
@@ -58,6 +93,28 @@ export class Tests extends Service {
           }
           resolve(documentList);
         });
+    });
+  }
+
+  async create(data: NewTestDto, params?: createApplication.Params): Promise<CreatedTestDto[] | CreatedTestDto> {
+    return new Promise((resolve, reject) => {
+
+      const insertableDocument: TestSchema = {
+        employeeIdentifier: data.employeeIdentifier,
+        testingCampaignID: new ObjectId(data.testingCampaignID),
+        creationTimestamp: new Date(),
+        status: TestStatus.CREATED,
+        result: TestResult.PENDING
+      };
+
+      this.Model.insertOne(insertableDocument).then(result => {
+        if (result.insertedCount !== 1) {
+          reject();
+        }
+
+        insertableDocument._id = result.insertedId;
+        resolve(<CreatedTestDto>insertableDocument);
+      });
     });
   }
 };
