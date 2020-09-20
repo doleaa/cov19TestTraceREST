6303630a6f8a8eb77e9e137d1bf73a756ce2d414
adolea <dolea15@gmail.com>
2020-09-20 15:45:26 +0100

    --remove randomSvc configuration

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,7 @@
 import { Application } from '../declarations';
 import tests from './tests/tests.service';
-import randomSvc from './random-svc/random-svc.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(tests);
-  app.configure(randomSvc);
 }
